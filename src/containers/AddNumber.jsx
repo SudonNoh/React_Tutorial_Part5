5dcea177209e0435e81647943d0f2c63866bacfe
sudonnoh <sudonnoh@gmail.com>
2021-12-17 16:24:53 +0900

    React Redux

--- src/containers/AddNumber.jsx
+++ src/containers/AddNumber.jsx
@@ -1,4 +1,19 @@
 import AddNumber from '../components/AddNumber';
+import { connect } from 'react-redux';
+import store from '../store';
+
+// 위 함수는 dispatch를 아래와 같이 실행시켜줌
+function mapReduxDispatchToReactProps(dispatch) {
+  return {
+    onClick: function(size) {
+      // store.dispatch > dispatch로 됨
+      dispatch({ type: 'INCREMENT', size: size });
+    },
+  };
+}
+
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+/*
 import React, { Component } from 'react';
 import store from '../store';
 
@@ -14,3 +29,4 @@ export default class extends Component {
     );
   }
 }
+*/
